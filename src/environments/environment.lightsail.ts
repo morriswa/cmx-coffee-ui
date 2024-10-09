d219a34b3cf222d8c6994a5793eb32c863018744
morriswa <william@morriswa.org>
2024-10-09 16:37:57 -0500

    CMX-6 + ci pipe

--- src/environments/environment.lightsail.ts
+++ src/environments/environment.lightsail.ts
@@ -2,3 +2,20 @@ export const environment = {
   APP_API_ENDPOINT:'https://www.morriswa.org/cmx/api/battleship',
   RUNTIME:'local'
 };
+
+export const AUTH0_CONFIG = {
+  domain: 'dev-9deub659.us.auth0.com',
+  clientId: 'JDy3gRSPBVcmJmlXKsMBaMwwIV7if2c3',
+  authorizationParams: {
+    audience: 'api://localhost',
+    scope: 'openid email cmx_coffee:admin',
+    redirect_uri: window.location.origin
+  },
+  // Specify configuration for the interceptor
+  httpInterceptor: {
+    allowedList: [
+      { uri: `${environment.APP_API_ENDPOINT}/*` },
+    ]
+  },
+  cacheLocation: "localstorage" as const
+};
