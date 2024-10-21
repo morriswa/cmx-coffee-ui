093d4016e69c06021461cd657bc0223d4f75167e
morriswa <william@morriswa.org>
2024-10-21 13:15:17 -0500

    stupid scroll window

--- src/app-customer-portal/pages/landing/customer-landing-page.component.ts
+++ src/app-customer-portal/pages/landing/customer-landing-page.component.ts
@@ -4,7 +4,8 @@ import {Component} from "@angular/core";
   selector: 'customer-landing-page',
   templateUrl: './customer-landing-page.component.html',
   styleUrl: './customer-landing-page.component.scss',
-  standalone: true
+  standalone: true,
+  host: {'class': 'app-body',}
 })
 export class CustomerLandingPageComponent {
 
