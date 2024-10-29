a7aebd36304bb5355a9398229ec9412a8c55b4a4
morriswa <william@morriswa.org>
2024-10-29 16:17:57 -0500

    CMX-51 # code review session 1

--- src/app-customer-portal/pages/landing/customer-landing-page.component.ts
+++ src/app-customer-portal/pages/landing/customer-landing-page.component.ts
@@ -1,10 +1,16 @@
 import {Component} from "@angular/core";
+import {NgOptimizedImage} from "@angular/common";
+
 
 @Component({
   selector: 'customer-landing-page',
   templateUrl: './customer-landing-page.component.html',
   styleUrl: './customer-landing-page.component.scss',
   standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  host: {'class': 'flex-child'}
 })
 export class CustomerLandingPageComponent {
 
