4aef87d091ad9dfd5d4ae393d3760b7562d1c9e6
Timothy Holmes <holmestimo11@gmail.com>
2024-11-03 19:52:08 -0600

    edited Coffee quiz

--- src/app-customer-portal/components/taste-questionnaire/coffee-questionnaire.component.ts
+++ src/app-customer-portal/components/taste-questionnaire/coffee-questionnaire.component.ts
@@ -27,7 +27,7 @@ export class CoffeeQuestionnaireComponent {
 
   ref = inject(DialogRef);
 
-  page: WritableSignal<number> = signal(1);
+  page: WritableSignal<number> = signal(0);
   forwardDisabled: Signal<boolean> = computed(()=>this.page()>=this.PAGE_COUNT);
   backwardDisabled: Signal<boolean> = computed(()=>this.page()<=1);
   pageOneCoffeeStrengthForm: CheckboxSelectorFormControl = new CheckboxSelectorFormControl([
@@ -64,17 +64,13 @@ export class CoffeeQuestionnaireComponent {
     { value: 'np', label: 'I love all coffee!!' },
   ]);
   pageSevenCoffeeToolForm: CheckboxSelectorFormControl = new CheckboxSelectorFormControl([
-    { value: 'espresso', label: 'Espresso' },
+    { value: 'espresso', label: 'Espresso Machine' },
     { value: 'drip', label: 'Drip' },
     { value: 'french', label: 'French Press' },
     { value: 'pour', label: 'Pour Over' },
     { value: 'perc', label: 'Percolator' },
     { value: 'cold', label: 'Cold Brew' },
     { value: 'air', label: 'Airopress' },
-  ])
-  pageEightForm: RadioButtonFormControl = new RadioButtonFormControl([
-    { value: 'fun', label: 'Fun' },
-    { value: 'notfun', label: 'Not fun' },
   ])
 
   forward() {
