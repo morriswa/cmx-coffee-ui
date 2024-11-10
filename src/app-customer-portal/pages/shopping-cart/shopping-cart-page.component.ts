d8b8ec10fe8becc77da4559c201bea8a5759e8b8
morriswa <william@morriswa.org>
2024-11-10 13:03:15 -0600

    + style shopping cart page

--- src/app-customer-portal/pages/shopping-cart/shopping-cart-page.component.ts
+++ src/app-customer-portal/pages/shopping-cart/shopping-cart-page.component.ts
@@ -1,11 +1,23 @@
 import {Component, inject, OnInit} from "@angular/core";
-import {ShoppingCartService} from "../../services/shopping-cart.service";
+import {ShoppingCartService} from "src/app-customer-portal/services/shopping-cart.service";
+import {CurrencyPipe, NgIf} from "@angular/common";
+import {FancyButtonComponent} from "src/components/fancy-button/fancy-button.component";
+import {RouterLink} from "@angular/router";
+import {LoaderComponent} from "src/components/loader/loader.component";
 
 
 @Component({
   selector: "app-shopping-cart-page",
   templateUrl: "./shopping-cart-page.component.html",
+  styleUrl: "./shopping-cart-page.component.scss",
   standalone: true,
+  imports: [
+    CurrencyPipe,
+    FancyButtonComponent,
+    RouterLink,
+    NgIf,
+    LoaderComponent
+  ],
   host: {'class': 'flex-child'}
 })
 export class ShoppingCartPageComponent implements OnInit {
--- src/app-customer-portal/services/shopping-cart.service.ts
+++ src/app-customer-portal/services/shopping-cart.service.ts
@@ -1,4 +1,4 @@
-import {inject, Injectable, Signal, signal, WritableSignal} from "@angular/core";
+import {computed, inject, Injectable, Signal, signal, WritableSignal} from "@angular/core";
 import {ApiClient} from "src/services/api-client.service";
 import {CartItem} from "../../types/product.type";
 
@@ -8,11 +8,34 @@ export class ShoppingCartService {
   api = inject(ApiClient);
 
 
-  _cart: WritableSignal<CartItem[]> = signal([]);
+  _cart: WritableSignal<CartItem[]|undefined> = signal(undefined);
+  _cartTotal: Signal<number> = computed(()=>{
+    const items = this._cart();
+    if (!items) return -1;
+    const prices = items.map(item=>item.sale_price*item.quantity);
+    let sum = 0;
+    for (const price of prices) sum += price;
+    return sum;
+  })
+  _cartItems: Signal<number> = computed(()=>{
+    const items = this._cart();
+    if (!items) return -1;
+    const itemCounts = items.map(item=>item.quantity);
+    let sum = 0;
+    for (const itemCount of itemCounts) sum += itemCount;
+    return sum;
+  })
 
-
-  get cart(): Signal<CartItem[]> {
+  get cart(): Signal<CartItem[]|undefined> {
     return this._cart;
+  }
+
+  get cartTotal(): Signal<number> {
+    return this._cartTotal;
+  }
+
+  get cartItems(): Signal<number> {
+    return this._cartItems;
   }
 
   addToCart(productId: number) {
